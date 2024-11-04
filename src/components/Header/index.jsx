f39c63670a61d934044c1a78ba39f5f708dfbce8
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-11-04 17:08:55 +0100

    fix: add NavLink

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,34 @@
 import styles from './index.module.scss';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Header() {
-    return (
-        <header className={`${styles.header} d-flex align-center justify-between`}>
-            <Link to="/">
-                <img src="logo.png" alt="" />
-            </Link>
+  return (
+    <header className={`${styles.header} d-flex align-center justify-between`}>
+      <Link to="/">
+        <img src="logo.png" alt="" />
+      </Link>
 
-            <ul className={`${styles.headerItems} d-flex`}>
-                <Link to="/" className={styles.headerItem}>
-                    Accueil
-                </Link>
-                <Link to="/about">A propos</Link>
-            </ul>
-        </header>
-    );
+      <ul className={`${styles.headerItems} d-flex`}>
+        <NavLink
+          end
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? `${styles.linkActive} ${styles.headerItem}`
+              : `${styles.headerItem}`
+          }
+        >
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? `${styles.linkActive}` : '')}
+        >
+          A propos
+        </NavLink>
+      </ul>
+    </header>
+  );
 }
 
 export default Header;
