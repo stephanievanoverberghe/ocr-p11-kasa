bd6a5fbab3fe0fcc3631b43c5d4453a567b92e8a
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-11-15 10:35:35 +0100

    fix: change height

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import styles from './App.module.scss';
-import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
