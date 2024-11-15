e869989af822be96ed8533b27406037d9d0876ef
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-11-15 10:39:16 +0100

    fix: change footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,17 +2,16 @@ import styles from './index.module.scss';
 import { Link } from 'react-router-dom';
 
 function Footer() {
-    return (
-        <footer className={styles.footer}>
-            <Link to="/">
-                <img src="/logo-white.png" alt="" />
-            </Link>
-            <div className={styles.footerParagraph}>
-                <p>© 2020 Kasa. All</p>
-                <p>rights reserved</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <Link to="/">
+        <img src="/logo-white.png" alt="" />
+      </Link>
+      <div className={styles.footerParagraph}>
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
